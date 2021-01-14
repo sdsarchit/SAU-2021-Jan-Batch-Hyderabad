0cb6bcbded3281998a321863445e8f80caaa8adf
sdsarchit <sdsarchit1@gmail.com>
2021-01-15 03:33:06 +0530

    2

---
+++ javascript-1/Add/add.js
@@ -0,0 +1,3 @@
+function add(){
+    document.getElementById('c').value=parseInt(document.getElementById('a').value)+parseInt(document.getElementById('b').value);
+}
---
+++ javascript-1/Alert/alert.js
@@ -0,0 +1,2 @@
+function sub(){
+alert('Hello World');}
